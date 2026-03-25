d88dacca7ae48b85b0944e78095733df788eddc4
Bueggi <christopher.buecklein@googlemail.com>
2026-03-25 13:00:51 +0100

    sign in fixed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/svelte";
+import { env } from "$env/dynamic/public";
 import type { auth } from "./server/auth";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:5173",
+    baseURL: env.PUBLIC_BETTER_AUTH_URL || (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:5173'),
     inferAdditionalFields: {} as typeof auth
 });
 
